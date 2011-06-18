f27cae00dbcc074248ef3c9da7e7e51b8b27a3bc
Dave Merwin <dave@davemerwin.com>
2011-06-17 17:05:18 -0700

    production db

--- gamelogic/settings/production.py
+++ gamelogic/settings/production.py
@@ -2,9 +2,9 @@ from base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'merwin_kidups',                      # Or path to database file if using sqlite3.
-        'USER': 'merwin_kidups',                      # Not used with sqlite3.
-        'PASSWORD': '5064b3fd',                  # Not used with sqlite3.
+        'NAME': 'merwin_gamelogic',                      # Or path to database file if using sqlite3.
+        'USER': 'merwin_gamelogic',                      # Not used with sqlite3.
+        'PASSWORD': '24e4838b',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
