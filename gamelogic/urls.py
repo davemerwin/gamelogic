ee1c14f65897e74fd58735267609da09e7f522f4
Dave Merwin <dave@davemerwin.com>
2011-06-17 17:42:14 -0700

    more python paths

--- gamelogic/urls.py
+++ gamelogic/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from story import views
+from gamelogic.story import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
