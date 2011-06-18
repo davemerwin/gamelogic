c7be2797c073662c0a8684c51fcbc3baa4c59d6e
Dave Merwin <dave@davemerwin.com>
2011-06-17 17:18:34 -0700

    change db password

--- gamelogic/settings/production.py
+++ gamelogic/settings/production.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'merwin_gamelogic',                      # Or path to database file if using sqlite3.
         'USER': 'merwin_gamelogic',                      # Not used with sqlite3.
-        'PASSWORD': '24e4838b',                  # Not used with sqlite3.
+        'PASSWORD': 'p8r3bl83',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
