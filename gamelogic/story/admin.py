db0a4a66729ef01f32d43baad83f5ac5cfce27ae
Dave Merwin <dave@davemerwin.com>
2011-07-15 10:49:12 -0700

    update to the question admin

--- gamelogic/story/admin.py
+++ gamelogic/story/admin.py
@@ -3,9 +3,13 @@ from story.models import Story, Question
     
 class StoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('key',)}
-    list_display = ('key','title', 'editorial_alpha', 'science_alpha', 'editorial_beta', 'science_beta', 'editorial_gold', 'science_gold',)
+    list_display = ('title', 'editorial_alpha', 'science_alpha', 'editorial_beta', 'science_beta', 'editorial_gold', 'science_gold',)
     list_editable = ('editorial_alpha', 'science_alpha', 'editorial_beta', 'science_beta', 'editorial_gold', 'science_gold',)
     list_filter = ('key','title', 'modified', 'editorial_alpha', 'science_alpha', 'editorial_beta', 'science_beta', 'editorial_gold', 'science_gold',)
     
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question','story_question_appears_on', 'story_question_answer_goes_to',)
+    list_filter = ('story_question_appears_on', 'story_question_answer_goes_to',)
+    
 admin.site.register(Story, StoryAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
