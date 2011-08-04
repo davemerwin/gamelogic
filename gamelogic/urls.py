231d8e16960fe5267d0bf461c1372478e692469b
Dave Merwin <dmerwin@orcasinc.com>
2011-08-04 16:51:24 -0700

    adding logon and logout templates

--- gamelogic/urls.py
+++ gamelogic/urls.py
@@ -11,6 +11,9 @@ urlpatterns = patterns('',
     
     (r'^comments/', include('django.contrib.comments.urls')),
     
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}),
+    
     url(r'^(?P<id>\d+)/edit/$', views.story_edit, name='story_edit'),
     url(r'^(?P<id>\d+)/$', views.story_detail, name='story_detail'),
     url(r'^$', views.story_list, name="story_list"),
